bb30c2b79be73bb9a8a295791d92251bd0de99b2
teser3000000 <92642256+teser3000000@users.noreply.github.com>
2023-05-30 21:50:04 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Вывод на экран всех нечетных чисел в диапозоне от 1 до 100:");
+for (int i = 0; i <= 100; i++) if (i % 2 != 0) Console.Write($" {i}");
+Console.WriteLine("\n");
+Console.Write("Первые 10 чисел Фибоначчи: 0 1");
+for (int i = 0, fibonacciNumbersFirst = 0, fibonacciNumbersTwelve = 1, resultOfMultiplication; i <= 7; i++)
+{
+    resultOfMultiplication = fibonacciNumbersFirst + fibonacciNumbersTwelve;
+    fibonacciNumbersFirst = fibonacciNumbersTwelve;
+    fibonacciNumbersTwelve = resultOfMultiplication;
+    Console.Write($" {resultOfMultiplication}");
+}
+
+//В какой день суммарный пробег лыжника превысит 100k
+Console.WriteLine("\n");
+
+for (double i = 2, mileage = 10; mileage <= 100; i++)
+{
+    mileage += mileage * (10f / 100);
+    Console.Write($"\n День {i} -- {Math.Round(mileage, 2)}");
+}
