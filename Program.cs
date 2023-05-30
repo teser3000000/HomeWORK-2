de9243ad5e89c7e0607045d959f94715a3d92032
teser3000000 <92642256+teser3000000@users.noreply.github.com>
2023-05-30 21:56:02 +0300

    c1

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Вывод на экран всех нечетных чисел в диапозоне от 1 до 100:");
 for (int i = 0; i <= 100; i++) if (i % 2 != 0) Console.Write($" {i}");
-Console.WriteLine("\n");
-Console.Write("Первые 10 чисел Фибоначчи: 0 1");
+
+Console.Write("\nПервые 10 чисел Фибоначчи: 0 1");
 for (int i = 0, fibonacciNumbersFirst = 0, fibonacciNumbersTwelve = 1, resultOfMultiplication; i <= 7; i++)
 {
     resultOfMultiplication = fibonacciNumbersFirst + fibonacciNumbersTwelve;
@@ -12,7 +12,6 @@ for (int i = 0, fibonacciNumbersFirst = 0, fibonacciNumbersTwelve = 1, resultOfM
 
 //В какой день суммарный пробег лыжника превысит 100k
 Console.WriteLine("\n");
-
 for (double i = 2, mileage = 10; mileage <= 100; i++)
 {
     mileage += mileage * (10f / 100);
